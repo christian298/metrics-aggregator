e9b92a41ffe60d428d27c5f059ff59b290c3f592
Christian Sandvoß <christian.sandvoss@gmail.com>
2020-06-17 19:35:39 +0200

    Removed unusewd code

--- models/metrics.go
+++ models/metrics.go
@@ -1,11 +1,5 @@
 package models
 
-type PerformanceMetrics struct {
-	FinishDocumentTime float32 `json:"finishDocumentTime"`
-	TTFB               float32 `json:"ttfb"`
-	FCP                float32 `json:"fcp"`
-}
-
 type Metric struct {
 	Name  string  `json:"name"`
 	Value float32 `json:"value"`
